a587987b9c4e53ada807a169bccdeea5419ddc85
Toni de la Torre <dlt@erni-espana.es>
2018-12-11 14:11:45 +0100

    Add project files.

---
+++ MyFridge.Client/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
+
+namespace MyFridge.Client
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
+    }
+}
